6a570e16af297d0c46252a89d3607e0c35a551a4
Sergey Volkov <js2me@outlook.com>
2025-02-26 15:35:51 +0300

    feat: add isShallowEqual utility function

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+import { isEqual } from 'lodash-es';
+
+import { AnyObject } from './utils/types';
+
+export const isShallowEqual = (a: unknown, b: unknown) => {
+  if (a === b) {
+    return true;
+  }
+
+  if (
+    typeof a !== 'object' ||
+    typeof b !== 'object' ||
+    a === null ||
+    b === null
+  ) {
+    return false;
+  }
+
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+
+  return (
+    isEqual(aKeys, bKeys) &&
+    aKeys.every((key) => (a as AnyObject)[key] === (b as AnyObject)[key])
+  );
+};
