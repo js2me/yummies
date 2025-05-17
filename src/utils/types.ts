7a05ac5e1b4b2ed566093c54515d33d76e7eaf5f
Sergey Volkov <js2me@outlook.com>
2025-05-17 21:13:08 +0300

    fix: utility types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -17,12 +17,12 @@ export type Nullable<T> = T | null;
 /**
  * Any object with any values
  */
-export type AnyObject = Record<string, any>;
+export type AnyObject = Record<keyof any, any>;
 
 /**
  * Empty object without properties
  */
-export type EmptyObject = Record<string, never>;
+export type EmptyObject = Record<keyof any, never>;
 
 export type AnyPrimitive = string | number | boolean | null | undefined;
 
@@ -47,9 +47,13 @@ export type Class<T, Args extends any[] = any[]> = new (...args: Args) => T;
 /**
  * Все свойства будут опциональны, в любую глубину
  */
-export type DeepPartial<T> = {
-  [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
-};
+export type DeepPartial<T> = T extends BrowserNativeObject
+  ? T
+  : {
+      [K in keyof T]?: ExtractObjects<T[K]> extends never
+        ? T[K]
+        : DeepPartial<T[K]>;
+    };
 
 export type PartialKeys<T, K extends keyof T> = Omit<T, K> &
   Partial<Pick<T, K>>;
@@ -121,3 +125,28 @@ export type Params<T extends (...args: any) => any> = T extends {
   : T extends (...args: infer P) => any
     ? P
     : never;
+
+export type Primitive =
+  | null
+  | undefined
+  | string
+  | number
+  | boolean
+  | symbol
+  | bigint;
+
+export type BrowserNativeObject = Date | FileList | File | Element | Node;
+
+export type NonUndefined<T> = T extends undefined ? never : T;
+
+export type LiteralUnion<T extends U, U extends Primitive> =
+  | T
+  | (U & {
+      _?: never;
+    });
+
+export type ExtractObjects<T> = T extends infer U
+  ? U extends object
+    ? U
+    : never
+  : never;
