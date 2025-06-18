e76c021fdc35336f464ca25e6cd46d917b4cd7ab
Sergey Volkov <js2me@outlook.com>
2025-06-18 15:43:58 +0300

    chore: add deprecation for FnValue

--- src/common.ts
+++ src/common.ts
@@ -1,8 +1,12 @@
-export type FnValue<TValue, TArgs extends any[] = []> =
-  | TValue
-  | ((...args: TArgs) => TValue);
+import { MaybeFn } from './utils/types.js';
+
+/**
+ * @deprecated use {MaybeFn} type
+ */
+export type FnValue<TValue, TArgs extends any[] = []> = MaybeFn<TValue, TArgs>;
 
 export const resolveFnValue = <TValue, TArgs extends any[] = []>(
+  // eslint-disable-next-line sonarjs/deprecation
   fn: FnValue<TValue, TArgs>,
   ...args: TArgs
 ) => {
