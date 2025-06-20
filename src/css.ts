ee9219ba5cfe4a89a2275b1f452882a75ad3c25d
Sergey Volkov <js2me@outlook.com>
2025-06-20 08:19:02 +0300

    feat: add cva css utility

--- src/css.ts
+++ src/css.ts
@@ -1,3 +1,4 @@
+import { cva as cvaLib } from 'class-variance-authority';
 import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -10,3 +11,13 @@ export const toRem = (px: number, remValue = 16) => `${px / remValue}rem`;
  * classNames/clsx но с примесями tailwind-merge
  */
 export const cx = (...args: Parameters<typeof clsx>) => twMerge(clsx(...args));
+
+/**
+ * Class Variance Authority но с примесями tailwind-merge
+ *
+ * https://cva.style/docs
+ */
+export const cva = ((...args: any[]) => {
+  const schema = cvaLib(...args);
+  return (...inputArgs: any[]) => twMerge(schema(...inputArgs));
+}) as any as typeof cvaLib;
