1cf24edc517012b2bb932273af2dc8a74d335f0f
Sergey Volkov <js2me@outlook.com>
2025-07-05 22:04:02 +0300

    feat: add unpackAsyncModule import function

--- src/imports.ts
+++ src/imports.ts
@@ -32,3 +32,21 @@ export const fetchLazyModule = async <T>(
   }
   throw lastError;
 };
+
+export type PackedAsyncModule<T> = Promise<T | { default: T }>;
+
+export const unpackAsyncModule = async <T>(
+  maybeModule: T | PackedAsyncModule<T>,
+): Promise<T> => {
+  if (maybeModule instanceof Promise) {
+    const data = (await maybeModule) as any;
+
+    if ((data as any).default) {
+      return (data as any).default;
+    } else {
+      return data;
+    }
+  }
+
+  return maybeModule;
+};
