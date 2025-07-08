895b0941d6d67ed52c205d7468e2627248c7036b
Sergey Volkov <js2me@outlook.com>
2025-07-08 12:25:50 +0300

    ci: fix vitest invalid compiler usage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,29 +1,16 @@
 import { defineConfig } from "vitest/config";
-import swc from 'unplugin-swc';
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [swc.vite({})], 
+  plugins: [react({
+    tsDecorators: true,
+  })],
   test: {
     globals: true,
     environment: "jsdom",
-    // pool: 'forks',
-    // poolOptions: {
-    //   forks: {
-    //     execArgv: [
-    //       '--cpu-prof',
-    //       '--cpu-prof-dir=test-runner-profile',
-    //       '--heap-prof',
-    //       '--heap-prof-dir=test-runner-profile'
-    //     ],
-
-    //     // To generate a single profile
-    //     singleFork: true,
-    //   },
-    // },
     coverage: {
       provider: 'istanbul', // or 'v8'
       include: ['src'],
-      exclude: ['src/preset'],
       reporter: [
         'text',
         'text-summary',
