9499c77f10f8f84c615321c4aea01e508b0e8740
Sergey Volkov <js2me@outlook.com>
2025-08-01 12:27:49 +0300

    fix: cva fn typings

---
+++ .changeset/red-sloths-love.md
@@ -0,0 +1,5 @@
+---
+"yummies": patch
+---
+
+fix cva typings
--- src/css.ts
+++ src/css.ts
@@ -1,6 +1,13 @@
 import { cva as cvaLib } from 'class-variance-authority';
-import clsx from 'clsx';
+import clsx, { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+
+type ClassProp = {
+  class?: ClassValue;
+  className?: ClassValue;
+};
+
+type StringToBoolean<T> = T extends 'true' | 'false' ? boolean : T;
 
 /**
  * Перевод значения в пикселях в rem строковое
@@ -12,6 +19,30 @@ export const toRem = (px: number, remValue = 16) => `${px / remValue}rem`;
  */
 export const cx = (...args: Parameters<typeof clsx>) => twMerge(clsx(...args));
 
+type ConfigSchema = Record<string, Record<string, ClassValue>>;
+type ConfigVariants<T extends ConfigSchema> = {
+  [Variant in keyof T]?: StringToBoolean<keyof T[Variant]> | null | undefined;
+};
+type ConfigVariantsMulti<T extends ConfigSchema> = {
+  [Variant in keyof T]?:
+    | StringToBoolean<keyof T[Variant]>
+    | StringToBoolean<keyof T[Variant]>[]
+    | undefined;
+};
+type Config<T> = T extends ConfigSchema
+  ? {
+      variants?: T;
+      defaultVariants?: ConfigVariants<T>;
+      compoundVariants?: (T extends ConfigSchema
+        ? (ConfigVariants<T> | ConfigVariantsMulti<T>) & ClassProp
+        : ClassProp)[];
+    }
+  : never;
+
+type Props<T> = T extends ConfigSchema
+  ? ConfigVariants<T> & ClassProp
+  : ClassProp;
+
 /**
  * Class Variance Authority но с примесями tailwind-merge
  *
@@ -20,6 +51,10 @@ export const cx = (...args: Parameters<typeof clsx>) => twMerge(clsx(...args));
 export const cva = ((...args: any[]) => {
   const schema = cvaLib(...args);
   return (...inputArgs: any[]) => twMerge(schema(...inputArgs));
-}) as any as typeof cvaLib;
+}) as any as <T>(
+  base?: ClassValue,
+  config?: Config<T>,
+) => (props?: Props<T>) => string;
 
 export type { VariantProps } from 'class-variance-authority';
+export type { ClassValue } from 'clsx';
