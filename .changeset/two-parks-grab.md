d5aba14d3fc40e2fe390d35b4cf0056c76e37cbc
Sergey Volkov <js2me@outlook.com>
2025-08-04 12:02:10 +0300

    feat!: rename CounterFn -> Counter

---
+++ .changeset/two-parks-grab.md
@@ -0,0 +1,5 @@
+---
+"yummies": patch
+---
+
+rename `CounterFn` -> `Counter` (mark deprecations)
--- src/complex/counter.ts
+++ src/complex/counter.ts
@@ -1,36 +1,42 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-export interface CounterFn<TProcessedValue = number> {
-  (): TProcessedValue;
+
+export interface Counter<TValue = number> {
+  (): TValue;
   counter: number;
-  value: TProcessedValue;
-  increment(): TProcessedValue;
-  decrement(): TProcessedValue;
+  value: TValue;
+  increment(): TValue;
+  decrement(): TValue;
   reset(): void;
 }
 
-export const createCounter = <TProcessedValue = number>(
-  processValue?: (value: number) => TProcessedValue,
+/**
+ * @deprecated use {`Counter`}. Will be removed in next major release
+ */
+export interface CounterFn<TValue = number> extends Counter<TValue> {}
+
+export const createCounter = <TValue = number>(
+  processValue?: (value: number) => TValue,
   initial: number = 0,
-): CounterFn<TProcessedValue> => {
+): Counter<TValue> => {
   const update = (counter: number) => {
-    fn.value = processValue?.(counter) ?? (counter as TProcessedValue);
+    fn.value = processValue?.(counter) ?? (counter as TValue);
     return fn.value;
   };
 
   const increment = () => update(++fn.counter);
   const decrement = () => update(--fn.counter);
 
-  const fn: CounterFn<TProcessedValue> = increment as any;
+  const fn: Counter<TValue> = increment as any;
 
   fn.increment = increment;
   fn.decrement = decrement;
 
   fn.reset = () => {
     fn.counter = initial;
-    fn.value = processValue?.(initial) ?? (initial as TProcessedValue);
+    fn.value = processValue?.(initial) ?? (initial as TValue);
   };
 
   fn.reset();
 
-  return fn as CounterFn<TProcessedValue>;
+  return fn as Counter<TValue>;
 };
