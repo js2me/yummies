15b8d1b442ca324b8812b7ec997cc69b3e4885c6
Sergey Volkov <js2me@outlook.com>
2025-09-20 00:36:05 +0300

    fix: missing css exports

---
+++ .changeset/cuddly-flies-find.md
@@ -0,0 +1,5 @@
+---
+"yummies": patch
+---
+
+fix missing css exports
--- scripts/generate-utility-types-file.ts
+++ scripts/generate-utility-types-file.ts
@@ -1,26 +1,10 @@
 #!/usr/bin/env tsx
 
-import { join } from "node:path";
-import { PackageJsonManager } from 'js2me-exports-post-build-script/utils/package-json-manager';
 import { $ } from "js2me-exports-post-build-script/utils";
 
 function generateUtilityTypesFile() {
-  $(`cp dist/utils/types.d.ts dist/utility-types.d.ts`, undefined, true);
-  $(`sed -i 's/^export type/type/' dist/utility-types.d.ts`, undefined, true);
-
-  const pckgJson = new PackageJsonManager(
-    join(process.cwd(), "./package.json")
-  );
-
-  console.log(join(process.cwd(), "./package.json"))
-  
-  pckgJson.data.exports['./utility-types'] = {
-    "types": "./dist/utility-types.d.ts",
-  }
-
-  pckgJson.syncWithFs();
-
-  console.log('generated utility types file OK');
+  $(`cp dist/utils/types.d.ts utility-types.d.ts`, undefined, true);
+  $(`sed -i 's/^export type/type/' utility-types.d.ts`, undefined, true);
 }
 
 // Run the script
