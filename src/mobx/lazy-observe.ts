e908005a4c37f93ebf5a1b91108ae04eef013029
Sergey Volkov <js2me@outlook.com>
2025-09-29 11:11:09 +0300

    fix: mobx util lazyObserve

--- src/mobx/lazy-observe.ts
+++ src/mobx/lazy-observe.ts
@@ -1,5 +1,11 @@
 import { onBecomeObserved, onBecomeUnobserved } from 'mobx';
 
+/**
+ * When ONE OF the properties is becomes observed then `onStart` function is called.
+ * WHen ALL properties are unobserved then `onEnd` function is called with the `metaData` that was returned by `onStart`.
+ *
+ * It uses `onBecomeObserved` and `onBecomeUnobserved` mobx hooks to perform lazy observation.
+ */
 export const lazyObserve = <TMetaData = void>({
   context,
   property,
@@ -15,25 +21,11 @@ export const lazyObserve = <TMetaData = void>({
 }) => {
   let timeoutId: ReturnType<typeof setTimeout> | undefined;
   let metaData: TMetaData | undefined;
+  const observingProps = new Set<string>();
   const properties = Array.isArray(property) ? property : [property];
-  let isObserving = false;
-
-  const start = () => {
-    if (isObserving) {
-      return;
-    }
-    isObserving = true;
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-      timeoutId = undefined;
-    }
-    metaData = onStart?.();
-  };
 
   const cleanup = () => {
-    if (!isObserving) {
-      return;
-    }
+    observingProps.clear();
 
     if (endDelay === false) {
       onEnd?.(metaData!, cleanup);
@@ -45,6 +37,7 @@ export const lazyObserve = <TMetaData = void>({
       clearTimeout(timeoutId);
       timeoutId = undefined;
     }
+
     timeoutId = setTimeout(() => {
       onEnd?.(metaData!, cleanup);
       timeoutId = undefined;
@@ -52,13 +45,43 @@ export const lazyObserve = <TMetaData = void>({
     }, endDelay);
   };
 
+  const start = (property: string) => {
+    const isAlreadyObserving = observingProps.size > 0;
+    observingProps.add(property);
+
+    if (isAlreadyObserving) {
+      return;
+    }
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = undefined;
+    }
+
+    metaData = onStart?.();
+  };
+
+  const stop = (property: string) => {
+    const isAlreadyNotObserving = !observingProps.size;
+
+    observingProps.delete(property);
+
+    const isObserving = observingProps.size > 0;
+
+    if (isAlreadyNotObserving || isObserving) {
+      return;
+    }
+
+    cleanup();
+  };
+
   properties.forEach((property) => {
     if (context) {
-      onBecomeObserved(context, property, start);
-      onBecomeUnobserved(context, property, cleanup);
+      onBecomeObserved(context, property, () => start(property));
+      onBecomeUnobserved(context, property, () => stop(property));
     } else {
-      onBecomeObserved(property, start);
-      onBecomeUnobserved(property, cleanup);
+      onBecomeObserved(property, () => start(property));
+      onBecomeUnobserved(property, () => stop(property));
     }
   });
 
