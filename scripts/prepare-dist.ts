6e457c5011dc42b6c9fe578d09785a45187df3bc
Sergey Volkov <js2me@outlook.com>
2025-10-07 09:38:56 +0300

    fix: build libs as dir exports (dts)

--- scripts/prepare-dist.ts
+++ scripts/prepare-dist.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env tsx
 
 
+import { readFileSync } from "fs";
+import { resolve } from "path";
 import { postBuildScript } from "sborshik/post-build-script";
+
+console.log(process.cwd())
+
+const tsconfig = JSON.parse(readFileSync(resolve(process.cwd(), './tsconfig.json')).toString())
 
 function prepareDist() {
   postBuildScript({
@@ -11,6 +17,23 @@ function prepareDist() {
     useBuildDirForExportsMap: true,
     filterExportsPathFn: (path) => {
       return path.startsWith('~');
+    },
+    onDone: (_, pckgJson) => {
+      const dirLibsPaths = (Object.entries(tsconfig.compilerOptions.paths) as unknown as [string, string[]]).filter((entry) => { 
+        return entry[1][0].endsWith('/index.ts'); 
+      }).map(entry => {
+        const libName = entry[0].replace(`${pckgJson.data.name}/`, '')
+        return `./${libName}`
+      })
+      const dirLibsSet = new Set(dirLibsPaths)
+      Object.keys(pckgJson.data.exports).forEach(exportPath=> {
+        if (dirLibsSet.has(exportPath) && typeof pckgJson.data.exports[exportPath] === 'object') {
+          pckgJson.data.exports[exportPath].types = `${exportPath}/index.d.ts`
+        }
+      })
+
+      pckgJson.syncWithFs();
+      console.log('dd', pckgJson.data, tsconfig);
     },
     filesToCopy: ['LICENSE', 'README.md'],
   })
