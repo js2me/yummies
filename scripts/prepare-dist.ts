e83545bc346435155d8199015833407db05b951a
Sergey Volkov <js2me@outlook.com>
2025-10-07 12:24:26 +0300

    chore: update build

--- scripts/prepare-dist.ts
+++ scripts/prepare-dist.ts
@@ -18,30 +18,7 @@ function prepareDist() {
     filterExportsPathFn: (path) => {
       return path.startsWith('~');
     },
-    onDone: (_, pckgJson) => {
-      const dirLibsPaths = (Object.entries(tsconfig.compilerOptions.paths) as unknown as [string, string[]]).filter((entry) => { 
-        return entry[1][0].endsWith('/index.ts'); 
-      }).map(entry => {
-        const libName = entry[0].replace(`${pckgJson.data.name}/`, '')
-        return `./${libName}`
-      })
-      const dirLibsSet = new Set(dirLibsPaths)
-      Object.keys(pckgJson.data.exports).forEach(exportPath=> {
-        if (typeof pckgJson.data.exports[exportPath] !== 'object') {
-          return;
-        }
-
-        if (!pckgJson.data.exports[exportPath].require) {
-          pckgJson.data.exports[exportPath].require = pckgJson.data.exports[exportPath].import.replace('.js', '.cjs')
-        }
-
-        if (dirLibsSet.has(exportPath)) {
-          pckgJson.data.exports[exportPath].types = `${exportPath}/index.d.ts`
-        }
-      })
-
-      pckgJson.syncWithFs();
-    },
+    addRequireToExportsMap: true,
     filesToCopy: ['LICENSE', 'README.md'],
   })
   
