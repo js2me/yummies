881e3ce2adc14c4cad77b251b734fa3a62118d1f
Sergey Volkov <js2me@outlook.com>
2025-11-05 22:59:19 +0300

    feat!: rewrite api for PubSub

---
+++ .changeset/full-ducks-exist.md
@@ -0,0 +1,5 @@
+---
+"yummies": patch
+---
+
+modified api for pubsub
--- src/complex/pub-sub.ts
+++ src/complex/pub-sub.ts
@@ -5,49 +5,44 @@ export type SubFn<PubArgs extends any[] = any[]> = (...args: PubArgs) => void;
  * through an event system. Subscribers can subscribe to events and receive notifications
  * when these events occur. The last published data can be accessed through the `data` property.
  */
-export class PubSub<PubArgs extends any[] = any[]> {
-  /**
-   * The last published data. This value can be null if no publication has occurred yet.
-   * It allows subscribers to receive the last published data when they subscribe.
-   */
-  data: PubArgs | null = null;
+export interface PubSub<PubArgs extends any[] = any[]> {
+  (...args: PubArgs): void;
 
   /**
    * An array of subscriber functions (sub) that will be called
    * when an event is published. Each subscriber must match the type SubFn,
    * taking the arguments that will be passed to it when the publisher calls pub.
    */
-  subs: SubFn<PubArgs>[] = [];
-
-  /**
-   * A function that is used to publish an event. When this function is called,
-   * all subscribers in the `subs` array will be called with the passed arguments.
-   */
-  pub(...args: PubArgs) {
-    this.data = args;
-    this.subs.forEach((sub) => {
-      sub(...args);
-    });
-  }
+  subs: SubFn<PubArgs>[];
 
   /**
    * A function to unsubscribe from events. When a subscriber function (sub) is passed,
    * it will be removed from the `subs` array, and will no longer receive notifications.
    */
-  unsub(sub: SubFn<PubArgs>) {
-    this.subs = this.subs.filter((it) => it !== sub);
-  }
-
+  unsub(sub: SubFn<PubArgs>): void;
   /**
    * A function to subscribe to events. When a subscriber function (sub) is passed,
    * it will be added to the `subs` array, and will receive notifications when the publisher calls pub.
    * Returns a function that can be used to unsubscribe from events.
    */
-  sub(sub: SubFn<PubArgs>) {
-    this.subs.push(sub);
-    return () => this.unsub(sub);
-  }
+  sub(sub: SubFn<PubArgs>): VoidFunction;
 }
 
-export const createPubSub = <PubArgs extends any[] = any[]>() =>
-  new PubSub<PubArgs>();
+export const createPubSub = <PubArgs extends any[] = any[]>() => {
+  const pubSub = ((...args: PubArgs) => {
+    pubSub.subs.forEach((sub) => {
+      sub(...args);
+    });
+  }) as PubSub<PubArgs>;
+
+  pubSub.subs = [];
+  pubSub.unsub = (sub: SubFn<PubArgs>) => {
+    pubSub.subs = pubSub.subs.filter((it) => it !== sub);
+  };
+  pubSub.sub = (sub: SubFn<PubArgs>) => {
+    pubSub.subs.push(sub);
+    return () => pubSub.unsub(sub);
+  };
+
+  return pubSub;
+};
